8b6821ade4f74532c9ceb13cbd1b169a5a14b6c5
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-09-11 23:06:33 +0900

    succeeded in defining route with parameter

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
+use rocket::http::RawStr;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/hello")]
-fn hello() -> &'static str {
-    "XXX"
+#[get("/blog/<id>")]
+fn blog(id: &RawStr) -> String {
+    format!("blog entry is: {}", id.as_str())
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, hello]).launch();
+    rocket::ignite().mount("/", routes![index, blog]).launch();
 }
