c20ceb5955207f985c6e2caceaec2f979172c0ec
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-09-11 23:08:24 +0900

    added category (second param) to the route (blog)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
-use rocket::http::RawStr;
-
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/blog/<id>")]
-fn blog(id: &RawStr) -> String {
-    format!("blog entry is: {}", id.as_str())
+#[get("/blog/<category>/<id>")]
+fn blog(category: String, id: String) -> String {
+    format!("blog entry is: [{}] {}", category, id)
 }
 
 fn main() {
