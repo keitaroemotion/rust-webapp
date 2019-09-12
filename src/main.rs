bd1cab521cb145c0d05dcb9600789ec785ccff7c
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-09-12 10:17:35 +0900

    routing more enriched

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,29 @@ fn index() -> &'static str {
 }
 
 #[get("/blog/<category>/<id>")]
-fn blog(category: String, id: String) -> String {
+fn blog_category_id(category: String, id: String) -> String {
     format!("blog entry is: [{}] {}", category, id)
 }
 
+#[get("/blog/<category>")]
+fn blog_category(category: String) -> String {
+    format!("blog category is: [{}]", category)
+}
+
+#[get("/blog")]
+fn blog() ->  &'static str {
+    "blog" 
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index, blog]).launch();
+    rocket::ignite().mount(
+                        "/",
+                        routes![
+                            index,
+                            blog,
+                            blog_category,
+                            blog_category_id
+                        ]
+                    )
+                    .launch();
 }
