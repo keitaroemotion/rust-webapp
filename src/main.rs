7c2fbaa94bd7a9836bf1f7618445f57b0fbd7488
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-09-12 11:19:47 +0900

    route with param success

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
+use rocket::request::Form;
+
+#[derive(FromForm)]
+struct Person {
+    category:  String,
+    entry: Option<u8>
+}
+
+#[get("/blog?<person..>")]
+fn blog(person: Option<Form<Person>>) -> String {
+    if let Some(person) = person {
+        if let Some(entry) = person.entry {
+            format!("Hello, {} year old categoryd {}!", entry, person.category)
+        } else {
+            format!("Hello {}!", person.category)
+        }
+    } else {
+        "We're gonna need a category, and only a category.".into()
+    }
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
-}
-
-#[get("/blog/<category>/<id>")]
-fn blog_category_id(category: String, id: String) -> String {
-    format!("blog entry is: [{}] {}", category, id)
-}
-
-#[get("/blog/<category>")]
-fn blog_category(category: String) -> String {
-    format!("blog category is: [{}]", category)
-}
-
-#[get("/blog")]
-fn blog() ->  &'static str {
-    "blog" 
 }
 
 fn main() {
@@ -26,9 +32,7 @@ fn main() {
                         "/",
                         routes![
                             index,
-                            blog,
-                            blog_category,
-                            blog_category_id
+                            blog
                         ]
                     )
                     .launch();
