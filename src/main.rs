5c19e74ef8f0b26f971dbfad37a17acb5434d148
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-09-13 19:11:36 +0900

    succeeded in mapping args to multiple arrays

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use rocket::State;
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 use std::env;
-use std::iter::Filter;
 use std::path::Path;
 use std::path::PathBuf;
 struct AssetsDir(String);
@@ -86,8 +85,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("args: {:?}", args);
     let re       = Regex::new(r"^(\w)+=(\w)+$").unwrap();
-    let mut iter = args.iter().filter(|x| re.is_match(x));
-    println!("{:?}", iter.next());
-    println!("{:?}", iter.next());
+    let mut iter = args
+                       .iter  ()
+                       .filter(|x| re.is_match(x))
+                       .map   (|x| x.split("=").collect::<Vec<&str>>());
+
+    println!("{:?}", iter.next().unwrap());
+    println!("{:?}", iter.next().unwrap());
     //launch_web();
 }
