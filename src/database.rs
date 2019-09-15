428928fd135db57a0ca3b99b036b723cb03a2122
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-09-15 15:05:47 +0900

    added test execution script

--- src/database.rs
+++ src/database.rs
@@ -1,13 +1,13 @@
 pub fn create_table() {
 }
-//
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//
-//    #[test]
-//    fn test_create_table()
-//    {
-//        assert_eq!(create_table(), 0)    
-//    }
-//}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_table()
+    {
+        //assert_eq!(create_table(), 0)    
+    }
+}
